e203f6e1eae7a43af76e51c8e7504f1e0c5ce640
Dustix1 <davidcz2016@gmail.com>
2021-04-12 09:19:58 +0200

    Piech updat
    yeet

--- Piech Bot/Program.cs
+++ Piech Bot/Program.cs
@@ -11,10 +11,12 @@ namespace Piech_Bot
         public static void Main(string[] args)  => new Program().MainAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
+
         public async Task MainAsync()
         {
             Console.Title = "Piech Bot";
             _client = new DiscordSocketClient();
+            _client.Ready += OnReady;
             _client.MessageReceived += CommandHandler;
             _client.Log += Log;
 
@@ -27,6 +29,22 @@ namespace Piech_Bot
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
+        }
+
+        private async Task OnReady()
+        {
+            Random rndPiechReady = new Random();
+            int randomPiechReady = rndPiechReady.Next(1, 1001);
+            if (randomPiechReady >= 0 && randomPiechReady <= 1000)
+            {
+                var PiechChannel = _client.GetChannel(831054488193335326) as IMessageChannel;
+                await PiechChannel.SendMessageAsync("@everyone Piech is here");
+            }
+            else
+            {
+                var PiechChannel = _client.GetChannel(831054488193335326) as IMessageChannel;
+                await PiechChannel.SendMessageAsync("I love it when you press F5 in Visual Studio 'cause it turns me on ;)");
+            }
         }
 
         private Task Log(LogMessage msg)
@@ -107,10 +125,10 @@ namespace Piech_Bot
                     Console.ForegroundColor = ConsoleColor.White;
                     message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " nice");
                 }
-            else
-            {
-                return Task.CompletedTask;
-            }
+                else
+                {
+                    return Task.CompletedTask;
+                }
             }
 
             if (message.Author.IsBot) //This ignores all commands from bots
@@ -143,7 +161,7 @@ namespace Piech_Bot
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.Write(" >> ");
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("ßrandompiech command ");
+                Console.Write("ßrandompiech ");
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine("random: " + random_Piech);
                 Console.ForegroundColor = ConsoleColor.White;
@@ -342,8 +360,9 @@ namespace Piech_Bot
             {
                 if (message.Author.Id == 385845659674345484)
                 {
-                    message.DeleteAsync();
-                    System.Environment.Exit(1);
+                    message.Channel.SendMessageAsync("no.. NO NOOOOOOOOOOOOOOOOOOOOOOOOOOO  *dies*");
+                    System.Threading.Thread.Sleep(3000);
+                    stopPiech();
                 }
                 else
                 {
@@ -392,6 +411,7 @@ namespace Piech_Bot
                     new EmbedFieldBuilder() { IsInline = true, Name = "**ßrandompiech**", Value = "Pošle random Piech obrázek"},
                     new EmbedFieldBuilder() { IsInline = true, Name = "**ßrandompiechvideo**", Value = "Pošle random Piech video"},
                     new EmbedFieldBuilder() { IsInline = true, Name = "**ßvizitka**", Value = "Vizitka od Piecha"},
+                    new EmbedFieldBuilder() { IsInline = true, Name = "**ßrandomucitel**", Value = "koňozmrdovyhovnazdržky"},
                     new EmbedFieldBuilder() { IsInline = true, Name = "**ßstop**", Value = "*Nemáš na tohle právo takže to ani nezkoušej*"},
                 })
                 .WithFooter("Cyberpiech s.r.o", "https://i.imgur.com/M6GOXYo.png")
@@ -400,9 +420,276 @@ namespace Piech_Bot
 
             } //                    ßhelp COMMAND
             //  CLASSIFIED COMMAND
+            else if (command.Equals("randomucitel") && message.Author.Id != 788873188472913951)
+            {
+                Random rndKonicek = new Random();
+                int random_konicek = rndKonicek.Next(1, 61);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt"));
+                Console.Write(" ");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write(message.Channel + " ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(message.Author);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(" >> ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("ßrandomucitel ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("random: " + random_konicek);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                if (random_konicek == 1)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " ...Dělejte to zezadu\n-Koníček 2020");
+                }
+                else if (random_konicek == 2)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " ...Můžet mě udělat... jo\n-Koníček 2020");
+                }
+                else if (random_konicek == 3)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " ...Bratr je obluda...\n-Štěpánová 2021");
+                }
+                else if (random_konicek == 4)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " ...Bratr je obluda...\n-Štěpánová 2021");
+                }
+                else if (random_konicek == 5)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " až budu mít čas, tak vám ho tam fouknu...\n-koníček 2021");
+                }
+                else if (random_konicek == 6)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " zakazuju vám to dělat hned..\n-koníček 2021");
+                }
+                else if (random_konicek == 7)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Já to nevysvětluju\n-koníček 2021");
+                }
+                else if (random_konicek == 8)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " když budete mít takové ty maminky...\n-Nerudová");
+                }
+                else if (random_konicek == 9)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Vem si motyku a di do vinohradu\n-koníček 2021");
+                }
+                else if (random_konicek == 10)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Občas tam fouknu plus\n-koníček 2021");
+                }
+                else if (random_konicek == 11)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " kluci pomožte mu, on tu kládu neunese sám...\n-koníček 2021");
+                }
+                else if (random_konicek == 12)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Trošku si pohrajem s tou odmocninou...\n-koníček 2021");
+                }
+                else if (random_konicek == 13)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Třináctku jste dělali na základce\n-koníček 2021");
+                }
+                else if (random_konicek == 14)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " nebojte budeme to dělat...\n-koníček 2021");
+                }
+                else if (random_konicek == 15)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Teď to budeme dělat...\n-koníček 2021");
+                }
+                else if (random_konicek == 16)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " a ted to budem dělat hodinu...\n-koníček 2021");
+                }
+                else if (random_konicek == 17)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Teď si slízni tu smetanu.\n-koníček 2021");
+                }
+                else if (random_konicek == 18)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " jak by něco polykala...\n-koníček 2021");
+                }
+                else if (random_konicek == 19)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " pošlete to do 1. úkolu\n-jedlička 2021");
+                }
+                else if (random_konicek == 20)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " tohle jsem dělal se žákama na základce...\n-koníček 2021");
+                }
+                else if (random_konicek == 21)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Já ti dám baby born.\n-koníček 2021");
+                }
+                else if (random_konicek == 22)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " zahrajem si na hantery\n-koníček 2021");
+                }
+                else if (random_konicek == 23)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Honza Kordula mě neslyší, nemá mikrofon...\n-Justová 2021");
+                }
+                else if (random_konicek == 24)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " rozlož mi dvanáctku...\n-koníček 2021");
+                }
+                else if (random_konicek == 25)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Oči mi vlhnou...\n-koníček 2021");
+                }
+                else if (random_konicek == 26)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Sežeň si kabel\n-koníček 2021");
+                }
+                else if (random_konicek == 27)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " devatenáctku nevykouříš...\n-koníček 2021");
+                }
+                else if (random_konicek == 28)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " jsem byl při chuti\n-koníček 2021");
+                }
+                else if (random_konicek == 29)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " já to dělám odzadu...\n-koníček 2021");
+                }
+                else if (random_konicek == 30)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " deseti minutové video, které trvá 15 minut, protože tam jsou sestřihy\n-koníček 2021");
+                }
+                else if (random_konicek == 31)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " já jsem mu vytáhl...\n-koníček 2021");
+                }
+                else if (random_konicek == 32)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " celou hodinu jsme mastili\n-koníček 2021");
+                }
+                else if (random_konicek == 33)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " srdíčko mi plesá\n-koníček 2021");
+                }
+                else if (random_konicek == 34)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " foukneš mu to na druhou stranu\n-koníček 2021");
+                }
+                else if (random_konicek == 35)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Lukáše nechci...\n-koníček 2021");
+                }
+                else if (random_konicek == 36)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Pořiďte si holku\n-Černoch 2021");
+                }
+                else if (random_konicek == 37)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " jsem z pětky\n-koníček 2021");
+                }
+                else if (random_konicek == 38)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Co se děje chlapci? Vy jste nějací pomotaní. Asi špatné záření...\n-Justová 2021");
+                }
+                else if (random_konicek == 39)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Nebudu vás dělat...\n-koníček 2021");
+                }
+                else if (random_konicek == 40)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " vždycky mi to vypočítejte protože já vám nevěřím že to umíte\n-koníček 2021");
+                }
+                else if (random_konicek == 41)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Teď vám nevěřím\n-koníček 2021");
+                }
+                else if (random_konicek == 42)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Dětska mi to dělali pravítkem\n-koníček 2021");
+                }
+                else if (random_konicek == 43)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " sestřička opálená\n-koníček 2021");
+                }
+                else if (random_konicek == 44)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " jak udělat hodinu atraktivní?\n-koníček 2021");
+                }
+                else if (random_konicek == 45)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Jo ty jsi ten kripl... Teda promiň disabled\n-Černoch 2021");
+                }
+                else if (random_konicek == 46)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Jsem pohodlný pán\n-koníček 2021");
+                }
+                else if (random_konicek == 47)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " zabít simona\n-Nerudová 2021");
+                }
+                else if (random_konicek == 48)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " lidé bez kružítka = chudí lidé\n-koníček 2021");
+                }
+                else if (random_konicek == 49)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Míša je kus chlapa\n-koníček 2021");
+                }
+                else if (random_konicek == 50)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Lukáši, ty si myslíš že se vrátíte do školy?\n-Halouzka 2021");
+                }
+                else if (random_konicek == 51)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " ty jseš víc na tvary když jseš chlap\n-koníček 2021");
+                }
+                else if (random_konicek == 52)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Když si zadáte třeba ten studentský antergránt\n-Štěpánová 2021");
+                }
+                else if (random_konicek == 53)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " ti hráči by museli být impotentní\n-koníček 2021");
+                }
+                else if (random_konicek == 54)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " vyměnil starou ženu za misku\n-učitelka na zsv 2021");
+                }
+                else if (random_konicek == 55)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Filip Urbanczik už mluví, slyšíte mě? - Ne, neslyším\n-Štěpánová 2021");
+                }
+                else if (random_konicek == 56)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " Chlapy se raději dívají na muže než na ženy\n-koníček 2021");
+                }
+                else if (random_konicek == 57)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " totálně rozmazlenej čupklik\n-Nerudová 2021");
+                }
+                else if (random_konicek == 58)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " adame, to tě uživí nějaké zelené breberky\n-koníček 2021");
+                }
+                else if (random_konicek == 59)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " E-brm-brm-ware\n-Prokop 2021");
+                }
+                else if (random_konicek == 60)
+                {
+                    message.Channel.SendMessageAsync(MentionUtils.MentionUser(message.Author.Id) + " měl jsem trochu máslo na hlavě\n-koníček 2021");
+                }
+
+            } //            ßrandomucitel Command
 
             return Task.CompletedTask;
 
+        }
+
+        void stopPiech()
+        {
+            System.Environment.Exit(1);
         }
     }
 }
